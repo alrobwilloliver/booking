79be1702db5746b715818c9da47978983e75a484
alrobwilloliver <alrobwilloliver@gmail.com>
2022-06-23 17:45:04 +0000

    fix booking repo import in booking module

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Booking } from '../entity/booking.entity';
 import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Booking])],
   controllers: [BookingController],
   providers: [BookingService],
 })
